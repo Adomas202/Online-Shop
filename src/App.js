4215caad8617ee6ecf3475147fa753ce64334e01
Poleninas <adomas202@gmail.com>
2018-09-11 23:20:30 +0300

    Initial commit
    First set up commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Product from './Components/Product';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Product />
+      </div>
+    );
+  }
+}
+
+export default App;
